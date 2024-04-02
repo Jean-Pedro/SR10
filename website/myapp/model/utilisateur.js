7123d488ea10819bc7c0c89c4b1fb4be7eb70a9f
julien pontoire <julien.pontoire@etu.utc.fr>
2024-04-02 14:35:33 +0200

    user

--- website/myapp/model/utilisateur.js
+++ website/myapp/model/utilisateur.js
@@ -27,13 +27,108 @@ module.exports = {
         });
     },  
 
-    creat: function (email, nom, prenom, num_tel, password, callback) {
-        date = Date.now();
-        sql = "INSERT INTO Utilisateur VALUES (NULL, ?, ?, ?, ?, ?, ?, 1, ?)";
-        db.query(sql, [email, nom, prenom, num_tel, date, date, password], function(err, result) {
+    generateHash: function (plaintextPassword, callback) {
+        bcrypt.hash(plaintextPassword, 10, function (err, hash) {
+            // call the function that Store hash in the database
+            callback(hash);
+        });
+    },
+
+    create: function (email, nom, prenom, num_tel, password, callback) {
+        var ladate = new Date();
+        var date_creation = ladate.getFullYear() + "-" + (ladate.getMonth() + 1) + "-" + ladate.getDate();
+        pass.generateHash(password, function (hash) {
+            rows = db.query("INSERT INTO Utilisateur (id_utilisateur, email, nom, prenom, num_tel, date_creation, last_login, statut, password) \
+            VALUES(NULL, ?, ?, ?, ?, ?, ?, 1, ?);", [email, nom, prenom, num_tel, date_creation, date_creation, hash], function (err, results) {
+                if (err) {
+                    callback(err, false);
+                } else {
+                    callback(err, true);
+                }
+            });
+        });
+    },
+
+
+    getbyID: function (id, callback) {
+        db.query("SELECT * FROM Utilisateur WHERE id_utilisateur= ?", id, function(err, result) {
             if (err) throw err;
-            console.log(result);
-        })
-        return false;
-    }
+            callback(results);
+        });
+    },
+
+    getInfos: function (email, callback) {
+        db.query("SELECT nom, prenom, num_tel FROM Utilisateur WHERE email = ?", email, function (err, results) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, results);
+            }
+        });
+    },
+
+    // latitude longitude
+
+    updateNom: function (email, new_nom, callback) {
+        rows = db.query("UPDATE Utilisateur SET nom = ? WHERE email =?", [new_nom, email], function (err, results) {
+            if (err) {
+                callback(err, false);
+            } else {
+                callback(err, true);
+            }
+        });
+    },
+
+    updatePrenom: function (email, new_prenom, callback) {
+        rows = db.query("UPDATE Utilisateur SET prenom = ? WHERE email = ?", [new_prenom, email], function (err, results) {
+            if (err) {
+                callback(err, false);
+            } else {
+                callback(err, true);
+            }
+        });
+    },
+
+    delete: function (email, callback) {
+        db.query("DELETE FROM Utilisateur WHERE email = ?", [email], function (err, results) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, "Utilisateur supprimé avec succès");
+            } 
+        });
+    },
+
+
+    TEST_MAIL: function (email_a_tester, callback) {
+        // Vérification de l'adresse e-mail 
+        // Format :
+        //  - 1 ou plusieurs caractères alphanumériques ou caractères spéciaux . _ - +
+        //  - @
+        //  - 1 ou plusieurs caractères alphanumériques ou caractères spéciaux . _ - +
+        //  - .
+        //  - 2 ou 3 caractères alphanumériques
+
+        var correct_email_test = /^([a-zA-Z0-9_\.\-+]+)@([a-zA-Z0-9_\.\-+]+)\.([a-zA-Z]{2,3})$/;
+        callback(correct_email_test.test(email_a_tester));
+    },
+
+
+    TEST_MDP: function (mdp1, callback) {
+        // 12 caractères minimum dont 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial parmis : &~"#'{}[]()-|`_\^@=/*-+.,?;:!<>€$£*
+        var correct_password_test = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[&~"#'{}[\]\(\)-|`_\^@=/*-+.,?;:!<>€$£*]).{12,}$/;
+        callback(correct_password_test.test(mdp1));
+    },
+
+    TEST_TEL: function (num_tel, callback) {
+        // Vérification du numéro de téléphone 
+        // Format : 
+        // - Commence par un + (optionnel) suivi de 1 à 3 chiffres (indicatif pays)
+        // - 0 ou 1 espace (optionnel)
+        // - 9 chiffres
+        var correct_tel_test = /(0|\+33)[1-9]( *[0-9]{2}){4}/;
+        callback(correct_tel_test.test(num_tel));
+    },
+
+
 }
