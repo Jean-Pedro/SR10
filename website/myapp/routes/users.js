1ca56b00fb0a830bb8e6ae5db002abfc9ea19294
julien pontoire <julien.pontoire@etu.utc.fr>
2024-04-03 12:05:44 +0200

    début ejs

--- website/myapp/routes/users.js
+++ website/myapp/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var userModel = require('../model/utilisateur')
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
-});
-
-module.exports = router;
+  });
+  router.get('/userslist', function (req, res, next) {
+  result=userModel.readall(function(result){
+  res.render('usersList', { title: 'List des utilisateurs', users: result });
+  });
+  });
+  module.exports = router;
