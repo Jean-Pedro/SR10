74dcad6e3a54af33421fc516bf10c2b1825d8db6
julien pontoire <julien.pontoire@etu.utc.fr>
2024-05-14 16:24:35 +0200

    tests users

---
+++ website/myapp/test/users.test.js
@@ -0,0 +1,54 @@
+const DB = require ("../model/db.js");
+const model = require ("../model/utilisateur.js");
+describe("Model Tests", () => {
+    beforeAll(() => {
+        // des instructions à exécuter avant le lancement des tests
+    });
+    afterAll((done) => {
+        function callback (err){
+            if (err) done (err);
+            else done();
+        }
+        DB.end(callback);
+    });
+    test ("read user",()=>{
+        nom=null;
+        function cbRead(resultat){
+            nom = resultat[0].nom;
+            expect(nom).toBe("Johnny");
+        }
+        model.read("johnny@gmail.com", cbRead);
+    });
+    test ("read all user",()=>{
+        nom1=null;
+        function cbRead(resultat){
+            nom1 = resultat[0].nom;
+            expect(nom1).toBe("René");
+        }
+        model.readall(cbRead);
+    });
+    test ("valid passwd",()=>{
+        valide=null;
+        function cbRead(resultat){
+            valide = resultat;
+            expect(vailde).toBeTruthy();
+        }
+        model.areValid(cbRead);
+    });
+    test ("get user by id",()=>{
+        nom=null;
+        function cbRead(resultat){
+            nom = resultat[0].nom;
+            expect(nom).toBe("René");
+        }
+        model.readall(1, cbRead);
+    });
+    test ("get user's info",()=>{
+        nom=null;
+        function cbRead(resultat){
+            nom = resultat[0].nom;
+            expect(nom).toBe("René");
+        }
+        model.readall("francoisrene@yahoo.fr", cbRead);
+    });
+})
